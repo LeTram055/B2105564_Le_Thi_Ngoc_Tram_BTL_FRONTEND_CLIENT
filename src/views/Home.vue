af15229e71e7fb22a9a130accd6ed7b7a55b5c5a
letram055 <tramb2105564@student.ctu.edu.vn>
2024-11-03 19:39:39 +0700

    cai dat trang home

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home-page">
+        <greeting :title="tilte"></greeting>
+    </div>
+</template>
+
+<script>
+import Greeting from "@/components/Layouts/Greeting.vue";
+export default {
+    data() {
+        return {
+            tilte: "Chào mừng bạn đến với webiste mượn sách!",
+        };
+    },
+    components: {
+        Greeting,
+    },
+}
+</script>
+
+<style scoped>
+
+.home-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 1.5rem;
+}
+</style>
