0f55cde648fc4d3084a5f3fde1fad3d6e23e797a
letram055 <tramb2105564@student.ctu.edu.vn>
2024-11-04 13:30:13 +0700

    cai dat chi tiet sach

---
+++ src/components/Book/BookDetail.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="row book-detail">
+        <div class="col-12 col-md-3">
+            <img :src="`${BASE_URL}${book.image}`" alt="book.name" class="book-image" />
+        </div>
+        <div class="col-12 col-md-9">
+            <h4 class="book-name text-justify">{{ book?.name }}</h4>
+            <p>Giá: <span class="font-weight-bold">{{ book?.price }}đ</span>
+            </p>
+            <p class="text-justify">Số lượng: {{ book?.quantity }}</p>
+            <p>Tác giả: {{ book?.author }}</p>
+            <p class="text-justify">Nhà xuất bản: {{ book?.publisherId?.name }}</p>
+            <p>Năm xuất bản: {{ book?.publishYear }}</p>
+            <p class="text-justify">Mô tả: {{ book?.description }}</p>
+            
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        book: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        console.log(import.meta.env.VITE_APP_BASE_URL);
+        return {
+            
+            BASE_URL: import.meta.env.VITE_APP_BASE_URL // Truy cập biến môi trường
+        }
+    },
+    methods: {
+    }
+};
+</script>
+
+<style scoped>
+.book-detail {
+    padding: 20px;
+    border: none;
+    border-radius: 5px;
+    transition: box-shadow 0.3s ease;
+    box-shadow: 0px 2px 10px rgb(226, 224, 224);
+}
+
+.book-image {
+    width: 200px;
+    min-height: 200px;
+    object-fit: cover;
+    padding-bottom: 10px;
+}
+
+.book-name {
+    font-size: 20px;
+}
+
+
+</style>
---
+++ src/views/BookDetail.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container">
+        <div class="row my-3 justify-content-center">
+            <greeting :title="title"></greeting>
+        </div>
+
+        <div class="row my-3 justify-content-center">
+            <book-detail :book="book"></book-detail>
+        </div>
+
+            <div class="row my-3 d-flex align-items-center">
+                <div class="col-6 d-flex justify-content-end">
+                    <input-quantity @changeQuantity="handleQuantityChange"></input-quantity>
+                    
+                </div>
+                <div class="col-6 d-flex justify-content-start">
+                    <btn :nameBtn="'Thêm vào giỏ'" @submit="addCart"></btn>
+                </div>
+            </div>
+        
+    </div>
+</template>
+
+<script>
+import Greeting from '@/components/Layouts/Greeting.vue';
+import BookDetail from '@/components/Book/BookDetail.vue';
+import Btn from "@/components/Layouts/Btn.vue";
+import InputQuantity from '@/components/Layouts/InputQuantity.vue';
+import { mapStores } from 'pinia';
+import authStore from '@/stores/auth.store';
+import cartService from '@/services/cart.service';
+import bookService from '@/services/book.service';
+
+export default {
+    computed: {
+        ...mapStores(authStore),
+    },
+    components: {
+        Greeting,
+        BookDetail,
+        Btn,
+        InputQuantity,
+    },
+    beforeMount: async function() {
+        const res = await bookService.getBookById(this.$route.params.id);
+        if (res.status == "success")
+            this.book = res.data;
+        else {
+            alert(res.message)
+        }
+    },
+    data() {
+        return {
+            title: 'Chi tiết sách',
+            book: {},
+            quantity: 1
+        }
+    },
+    methods: {
+        async addCart() {
+            if (!this.authStore.getUser) {
+                alert("Vui lòng đăng nhập để thêm sách vào giỏ hàng")
+                // this.$router.push({
+                //     name: "login",
+                // })
+                return
+            }
+            
+            const response = await cartService.addToCart(this.authStore.getUser._id, this.book._id, this.quantity);
+            alert(response.message)
+        },
+        handleQuantityChange(value) {
+            this.quantity = value
+        },
+    },
+};
+</script>
+
+<style scoped>
+</style>
